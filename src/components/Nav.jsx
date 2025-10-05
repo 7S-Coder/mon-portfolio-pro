21b046ed65d91e216a60c6951246321738176573
7S-Codere <dylan.e33@hotmail.fr>
2025-10-05 18:42:27 +0200

    create Home and Nav.jsx

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav>
+      <h1>Dylan Eglantine</h1>
+      <ul>
+        <li><a href="#skills">Compétences</a></li>
+        <li><a href="#projects">Mes projets</a></li>
+        <li><a href="#certifications">Formations et certifications</a></li>
+        <li><a href="#approach">Mon approche</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+} export default Nav;
