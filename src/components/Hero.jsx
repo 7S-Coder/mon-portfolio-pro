23b00032fd47be4861b0b24608788ec6546f5de5
7S-Codere <dylan.e33@hotmail.fr>
2025-10-05 18:50:04 +0200

    create hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+function Hero() {
+  return (
+    <section>
+      <h2>Bienvenue sur mon portfolio</h2>
+      <p>
+        <em>Je conçois, teste et déploie des applications web performantes et fiables.</em>
+        <p>Développeur Full Stack, 
+        Testeur Certifié ISTQB et passionné par l’automatisation et la qualité logicielle</p>
+        <div className="btn-gallery">
+          <button>Voir mes projets</button>
+          <button>Télécharger mon CV</button>
+          <button>Me contacter</button>
+        </div>
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
-import Nav from '../components/Nav'
+import Nav from '../components/Nav';
+import Hero from '../components/Hero';
 
 function Home() {
 
     return (
         <>
             <Nav />
+            <main>
+                <Hero />
+            </main>
         </>
     )
 }
